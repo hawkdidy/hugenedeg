34c558c166ed2c9b02503f8c9cc0d0424d4e1970
hawkdidy <mohamedhakim.elakhrass@student.kuleuven.be>
2016-02-19 10:00:04 +0100

    Update
    Changed the method of filtering.

--- R/filter.R
+++ R/filter.R
@@ -13,15 +13,14 @@
 #filtering of genes, removing all control probes and seeing what is filtered 
 filterc <- function(normData){
   
-con <- db(pd.hugene.2.0.st)
+eset <- getMainProbes(eset)
 
-probes.control <- dbGetQuery(con, "select fsetid from featureSet where
-                             type in ('3', '5','6','7','8','9','10','11','12','13','14','15','16','17');")[,1]
-
-normData.filtered <- nsFilter(normData, require.entrez=FALSE, remove.dupEntrez=FALSE, feature.exclude = probes.control)
+normData.filtered <- nsFilter(eset, require.entrez=FALSE, remove.dupEntrez=FALSE, feature.exclude = probes.control)
 
 return(normData.filtered$filter.log)
 
 assign("normData",normData.filtered, envir = .GlobalEnv)
 
 }
+
+
